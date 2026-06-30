e0c54760d912cbedb6c3f2e8b1e85ffbe213b7b5
Ahmad Ridwan <ahmadridwan058@gmail.com>
2026-06-30 18:25:28 +0700

    perbaikan icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ahmad Ridwan | Full-Stack Web Developer</title>
+    <link rel="icon" href="{{ asset('favicon.png?v=2') }}" type="image/png">
     
     <!-- Load Tailwind CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
